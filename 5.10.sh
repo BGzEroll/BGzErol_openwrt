89129e4bb689a6fa1ef75fc78bd3d88f550ad7c5
BGzEroll <104726856+BGzEroll@users.noreply.github.com>
2022-06-10 17:25:55 +0800

    Update 5.10.sh

--- 5.10.sh
+++ 5.10.sh
@@ -11,8 +11,8 @@
 #
 
 # mv -f neihe/Makefile ./target/linux/x86/Makefile
-# sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
-# sed -i 's/KERNEL_TESTING_PATCHVER:=5.15/KERNEL_TESTING_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=5.15/KERNEL_TESTING_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
