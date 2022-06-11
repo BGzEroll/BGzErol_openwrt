db516b473c5f6866ed929ba13f37b46b7647456a
BGzEroll <104726856+BGzEroll@users.noreply.github.com>
2022-06-12 00:58:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,9 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.114.1/g' package/base-files/files/bin/config_generate
-sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
-sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
-sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt by L '/g" ./package/lean/default-settings/files/zzz-default-settings
 sed -i "s/hostname='OpenWrt'/hostname='OpenWrt_BGzErol'/g" ./package/base-files/files/bin/config_generate
-
-# rm -rf ./package/lean/luci-theme-argon
