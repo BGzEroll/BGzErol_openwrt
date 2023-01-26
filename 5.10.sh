7292c007490de7b5625993b540ad15420a4025f7
BGzEroll <104726856+BGzEroll@users.noreply.github.com>
2023-01-26 18:00:03 +0800

    Update 5.10.sh

--- 5.10.sh
+++ 5.10.sh
@@ -11,6 +11,7 @@
 #
 
 # mv -f neihe/Makefile ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
