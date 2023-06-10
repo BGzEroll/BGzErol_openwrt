ff652896f6eb67e35b351fcaac9780bdfd07d06a
BGzErol <104726856+BGzEroll@users.noreply.github.com>
2023-06-10 10:25:53 +0800

    Update mainrouter_c.sh

--- mainrouter_c.sh
+++ mainrouter_c.sh
@@ -11,5 +11,5 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.50/g' package/base-files/files/bin/config_generate
 #sed -i "s/hostname='OpenWrt'/hostname='OpenWrt_BGzErol'/g" ./package/base-files/files/bin/config_generate
